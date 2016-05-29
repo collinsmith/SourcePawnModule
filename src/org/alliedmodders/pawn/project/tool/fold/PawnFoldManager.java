f7ead1bf4986273546a70cd0f007e8b39d941903
Collin Smith <collinsmith70@gmail.com>
2016-05-29 15:47:17 -0700

    PawnFoldManager now maintains a collection of all created Folds (to be used when updating hierarchy) Changed code block folding to block template Code blocks will now only fold at the root level (i.e., global functions, arrays, etc)

--- src/org/alliedmodders/pawn/project/tool/fold/PawnFoldManager.java
+++ src/org/alliedmodders/pawn/project/tool/fold/PawnFoldManager.java
@@ -1,6 +1,8 @@
 package org.alliedmodders.pawn.project.tool.fold;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Deque;
 import javax.swing.event.DocumentEvent;
 import javax.swing.text.BadLocationException;
@@ -14,6 +16,7 @@ import org.netbeans.api.lexer.Token;
 import org.netbeans.api.lexer.TokenHierarchy;
 import org.netbeans.api.lexer.TokenSequence;
 import org.netbeans.spi.editor.fold.FoldHierarchyTransaction;
+import org.netbeans.spi.editor.fold.FoldInfo;
 import org.netbeans.spi.editor.fold.FoldManager;
 import org.netbeans.spi.editor.fold.FoldOperation;
 import org.openide.filesystems.FileObject;
@@ -28,6 +31,7 @@ public class PawnFoldManager implements FoldManager {
     private FoldOperation operation;
     private FileObject file;
     private boolean first;
+    private Collection<FoldInfo> foldInfo = new ArrayList<>();
 
     @Override
     public void init(FoldOperation operation) {
@@ -44,7 +48,9 @@ public class PawnFoldManager implements FoldManager {
         Deque<Integer> bracesMatcher = new ArrayDeque<>();
         
         int start = 0;
+        int end = 0;
         int offset = 0;
+        Fold fold = null;
         FoldType type = null;
         Token<PawnTokenId> token;
         while (tokenSequence.moveNext()) {
@@ -55,28 +61,32 @@ public class PawnFoldManager implements FoldManager {
                     case DOC_COMMENT:
                         type = DOC_COMMENT_FOLD_TYPE;
                         start = offset;
-                        operation.addToHierarchy(
+                        end = start + token.length();
+                        fold = operation.addToHierarchy(
                                 type,
                                 start,
-                                start + token.length(),
+                                end,
                                 null,
                                 FoldTemplate.DEFAULT_BLOCK,
                                 "/** Documentation ... */",
                                 null,
                                 transaction);
+                        foldInfo.add(FoldInfo.range(start, end, type));
                         break;
                     case BLOCK_COMMENT:
                         type = COMMENT_FOLD_TYPE;
                         start = offset;
-                        operation.addToHierarchy(
+                        end = start + token.length();
+                        fold = operation.addToHierarchy(
                                 type,
                                 start,
-                                start + token.length(),
+                                end,
                                 null,
                                 FoldTemplate.DEFAULT,
                                 "/* ... */",
                                 null,
                                 transaction);
+                        foldInfo.add(FoldInfo.range(start, end, type));
                         break;
                     case LBRACE:
                         bracesMatcher.addLast(offset);
@@ -84,19 +94,24 @@ public class PawnFoldManager implements FoldManager {
                     case RBRACE:
                         if (bracesMatcher.isEmpty()) {
                             break;
+                        } else if (bracesMatcher.size() > 1) {
+                            bracesMatcher.removeLast();
+                            break;
                         }
                         
                         type = CODE_BLOCK;
                         start = bracesMatcher.removeLast();
-                        operation.addToHierarchy(
+                        end = offset + token.length();
+                        fold = operation.addToHierarchy(
                                 type,
                                 start,
-                                offset + token.length(),
+                                end,
                                 null,
-                                FoldTemplate.DEFAULT,
+                                FoldTemplate.DEFAULT_BLOCK,
                                 "{ ... }",
                                 null,
                                 transaction);
+                        foldInfo.add(FoldInfo.range(start, end, type));
                         break;
                 }
             } catch (BadLocationException e) {
@@ -119,7 +134,6 @@ public class PawnFoldManager implements FoldManager {
 
     @Override
     public void removeEmptyNotify(Fold fold) {
-        removeDamagedNotify(fold);
     }
 
     @Override
