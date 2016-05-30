2e7b077bce642e8156a34419677ecd204b6a5f91
Collin Smith <collinsmith70@gmail.com>
2016-05-30 00:20:32 -0700

    Added additional constructor which takes a lookup delegate that will be used in place of the source node lookup

--- src/org/alliedmodders/pawn/project/node/FileFilteredNode.java
+++ src/org/alliedmodders/pawn/project/node/FileFilteredNode.java
@@ -9,9 +9,22 @@ import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
 import org.openide.nodes.FilterNode;
 import org.openide.nodes.Node;
+import org.openide.util.Lookup;
+import org.openide.util.lookup.ProxyLookup;
 
 public class FileFilteredNode extends FilterNode {
 
+    public FileFilteredNode(Node original, FileFilter fileFilter) {
+        super(original, new FileFilteredChildren(original, fileFilter));
+    }
+    
+    public FileFilteredNode(Node original, Lookup delegate,
+            FileFilter fileFilter) {
+        super(original,
+                new FileFilteredChildren(original, fileFilter),
+                new ProxyLookup(delegate, original.getLookup()));
+    }
+    
     static class FileFilteredChildren extends FilterNode.Children {
 
         private final FileFilter fileFilter;
@@ -30,7 +43,7 @@ public class FileFilteredNode extends FilterNode {
         protected Node[] createNodes(Node key) {
             List<Node> result = new ArrayList<>();
             for (Node node : super.createNodes(key)) {
-                FileObject fileObject = (FileObject)node.getLookup().lookup(FileObject.class);
+                FileObject fileObject = node.getLookup().lookup(FileObject.class);
                 if (fileObject != null) {
                     File file = FileUtil.toFile(fileObject);
                     if (fileFilter.accept(file)) {
@@ -42,10 +55,6 @@ public class FileFilteredNode extends FilterNode {
             return result.toArray(new Node[0]);
         }
 
-    }
-
-    public FileFilteredNode(Node original, FileFilter fileFilter) {
-        super(original, new FileFilteredChildren(original, fileFilter));
     }
     
 }
